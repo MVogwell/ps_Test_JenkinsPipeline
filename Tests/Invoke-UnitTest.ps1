26ac071525883c3b17d4b71520d117fb537c1672
Martin Vogwell <martin.vogwell@ultra-electronics.com>
2023-01-06 06:54:06 +0000

    Removed spaces that were failing script analyzer tests

--- Tests/Invoke-UnitTest.ps1
+++ Tests/Invoke-UnitTest.ps1
@@ -10,14 +10,14 @@ Import-Module Pester
 
 if ((Get-Module pester).Version.Major -ge 5) {
 	Write-Output "`t+++ Using Pester v5 or above"
-	
+
 	$objPesterConfig = [PesterConfiguration]::Default
 	$objPesterConfig.Run.Exit = $true
 	$objPesterConfig.Run.Throw = $true
 	$objPesterConfig.TestResult.Enabled = $true
 	$objPesterConfig.TestResult.OutputPath = $OutputFile
 	$objPesterConfig.Output.Verbosity = "Detailed"
-	
+
 	Invoke-Pester -Configuration $objPesterConfig -WarningAction "SilentlyContinue"
 }
 else { # Using an older version of Pester
